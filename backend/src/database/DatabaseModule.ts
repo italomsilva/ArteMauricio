ebaedb562b043a635099d02e9a2d2a5de0786386
italomsilva <italoroyale13@gmail.com>
2024-07-24 14:39:23 -0300

    feat: add Docker setup for project environment

--- backend/src/database/DatabaseModule.ts
+++ backend/src/database/DatabaseModule.ts
@@ -1,16 +1,18 @@
 import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { Module } from "@nestjs/common";
-
+import { ConfigModule, ConfigService } from "@nestjs/config";
 @Module({
-  imports:[TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: parseInt(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    autoLoadEntities:true,
-    synchronize: true,  
-  })]
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DOCKER_DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities:true,
+      synchronize: true,      
+    }),
+  ],
 })
 export class DatabaseModule{}
