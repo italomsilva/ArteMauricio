b476e8530f218873ac43db77dc313af5d3a106d5
italomsilva <italoroyale13@gmail.com>
2024-07-31 14:45:29 -0300

    fix:get product

--- backend/src/core/usecases/product/GetProduct.ts
+++ backend/src/core/usecases/product/GetProduct.ts
@@ -1,20 +1,24 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { Product } from "../../domain/entities/Product";
-import { ProductRepository } from "../../domain/repositories/ProductRepository";
-import { CategoryRepository } from "src/core/domain/repositories/CategoryRepository";
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { ProductRepository } from '../../domain/repositories/ProductRepository';
+import { ProductImageRepository } from 'src/core/domain/repositories/ProductImageRepository';
+import { ProductCategoryRepository } from 'src/core/domain/repositories/ProductCategoryRepository';
+import { productFormatter } from 'src/core/utils/formatters/ProductFormatter';
 @Injectable()
-export class GetProductUseCase{
-    constructor(
-        @Inject('productRepository') private productRepository:ProductRepository,
-        @Inject('categoryRepository') private categoryRepository:CategoryRepository
-    ){}
+export class GetProductUseCase {
+  constructor(
+    @Inject('productRepository')
+    private readonly productRepository: ProductRepository,
+    @Inject('productImageRepository')
+    private readonly productImageRepository: ProductImageRepository,
+    @Inject('productCategoryRepository')
+    private readonly productCategoryRepository: ProductCategoryRepository,
+  ) {}
 
-    async execute(id:string):Promise<any>{
-        const product= await this.productRepository.findById(id);
-        const categories = await this.categoryRepository.findAll();
-        return {
-            product,
-            categories:categories
-        }
-    }
+  async execute(id: string): Promise<any> {
+    const product = await this.productRepository.findById(id);
+    if(!product) throw new NotFoundException('PRODUCT NOT FOUND');
+    const productCategories = await this.productCategoryRepository.findAll(product.id);
+    const productImages = await this.productImageRepository.findAll(product.id);
+    return productFormatter({product, categories: productCategories, images: productImages})
+  }
 }
