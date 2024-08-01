d2cfbdf30718801a5c206888b54aa6dabbd34d0c
italomsilva <italoroyale13@gmail.com>
2024-08-01 11:06:26 -0300

    feat: add mainPhoto to product and sql commands

--- backend/src/database/commands/insert.sql
+++ backend/src/database/commands/insert.sql
@@ -1,9 +1,9 @@
 -- Inserir dados na tabela products
-INSERT INTO products (id, title, price, description, created_at, updated_at)
+INSERT INTO products (id, title, price, description, created_at, updated_at, main_photo)
 VALUES
-    ('1', 'Apoiador de Celular', 9.99, 'Um suporte conveniente para seu celular', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    ('2', 'Mostruário', 19.99, 'Um mostruário para exibir itens', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    ('3', 'Incensário', 14.99, 'Um queimador decorativo para varetas de incenso', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+    ('1', 'Apoiador de Celular', 9.99, 'Um suporte conveniente para seu celular', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'https://apoiador'),
+    ('2', 'Mostruário', 19.99, 'Um mostruário para exibir itens', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'https://mostruario'),
+    ('3', 'Incensário', 14.99, 'Um queimador decorativo para varetas de incenso', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'https://insensario');
 
 -- Inserir dados na tabela categories
 INSERT INTO categories ( name, product_count)
