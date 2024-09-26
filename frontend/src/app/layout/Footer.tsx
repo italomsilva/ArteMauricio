09a7c98edf2f7affb9e8f0c8fd9f0e9089394426
italomsilva <italoroyale13@gmail.com>
2024-09-26 15:02:03 -0300

    style footer

--- frontend/src/app/layout/Footer.tsx
+++ frontend/src/app/layout/Footer.tsx
@@ -4,12 +4,16 @@ import { FaInstagram, FaWhatsapp, FaMailBulk } from "react-icons/fa";
 export default function Footer(){
     return(
         <footer className={styles.footer}>
-            <h2>Siga-nos nas redes sociais</h2>
-            <i><FaInstagram/></i>
-            <h2>Contate-nos</h2>
-            <i><FaWhatsapp/></i>
-            <i><FaMailBulk/></i>
+            <div className={styles.st}>
+                <h2>Siga-nos nas redes sociais</h2>
+                <i><FaInstagram/></i>
+                <h2>Contate-nos</h2>
+                <i><FaWhatsapp/></i>
+                <i><FaMailBulk/></i>
+            </div>
+            <div className={styles.nd}>
             <p>Website feito por Dev.Italo</p>
+            </div>
         </footer>
     )
 }
