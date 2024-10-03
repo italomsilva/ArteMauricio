bb13a261a2afb61c3855817f4a0631ac80b5d163
italomsilva <italoroyale13@gmail.com>
2024-10-03 15:57:22 -0300

    feat: contact page and input component

---
+++ frontend/src/app/(routes)/contact/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import ContactPage from "@/app/pages/contact_page/ContactPage" 
+
+export default function Page(){
+    return(
+        <main style={{display: 'flex', blockSize:'100vh',alignItems: 'center', justifyContent: "center"}}>
+            <ContactPage/>
+        </main>
+    )
+}
---
+++ frontend/src/app/(routes)/contact/thanks/page.tsx
@@ -0,0 +1,7 @@
+export default function Page(){
+    return(
+        <div style={{display: 'flex', blockSize:'100vh',alignItems: 'center', justifyContent: "center"}}>
+            <h1>Thank you</h1>
+        </div>
+    )
+}
