29f2a2a4ecdc381493b007c50c0180507e7d2590
italomsilva <italoroyale13@gmail.com>
2025-02-27 09:52:07 -0300

    update

--- backend/src/modules/products/controllers/ImageController.ts
+++ backend/src/modules/products/controllers/ImageController.ts
@@ -1,65 +1,42 @@
-import { Body, Controller, Delete, Patch, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Patch,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AddImageUseCase } from 'src/core/usecases/product/images/AddImage';
 import { ChangeImageOrderUseCase } from 'src/core/usecases/product/images/ChangeImageOrder';
 import { DeleteImageUseCase } from 'src/core/usecases/product/images/DeleteImage';
 import { FileInterceptor } from '@nestjs/platform-express';
 
-@Controller('products')
-export class ProductController {
-  constructor(private readonly addImageUseCase: AddImageUseCase) {}
-
-  @Post('add-image')
-  @UseInterceptors(FileInterceptor('file'))
-  async addImage(
-    @Body() body: { productId: string; imageOrder: number },
-    @UploadedFile() file: Express.Multer.File,
-  ) {
-    const input = {
-      productId: body.productId,
-      imageOrder: body.imageOrder,
-      file, 
-    };
-    return await this.addImageUseCase.execute(input);
-  }
-}
-
-
-@Controller('product')
+@Controller('product/image')
 export class ImageController {
   constructor(
     private readonly addImageUseCase: AddImageUseCase,
     private readonly changeImageOrderUseCase: ChangeImageOrderUseCase,
     private readonly deleteImageUseCase: DeleteImageUseCase,
   ) {}
-
-  @Post('add-image')
-  @UseInterceptors(FileInterceptor('file')) // O nome do campo no formulário deve ser 'file'
-  async addImage(
-    @Body() body: { productId: string; imageOrder: number },
-    @UploadedFile() file: Express.Multer.File,
-  ) {
-    console.log('Received File:', file); // Debugando o arquivo recebido
-    console.log('Received Body:', body); // Debugando o corpo da requisição
-
-    if (!file) {
-      throw new Error('No file uploaded');
-    }
-
+  @Post('add')
+  @UseInterceptors(FileInterceptor('file'))
+  async addImage(@Body() body: any, @UploadedFile() file: Express.Multer.File) {
     const input = {
       productId: body.productId,
       imageOrder: body.imageOrder,
-      file, // O arquivo enviado será automaticamente mapeado para a variável 'file'
+      file,
     };
-
     return await this.addImageUseCase.execute(input);
   }
-  @Patch('change-image-order')
+
+  @Patch('change-order')
   async changeOrder(@Body() body): Promise<any> {
     const result = await this.changeImageOrderUseCase.execute(body);
     return result;
   }
 
-  @Delete('delete-image')
+  @Delete('delete')
   async deleteImage(@Body() body): Promise<any> {
     const result = await this.deleteImageUseCase.execute(body);
     return result;
