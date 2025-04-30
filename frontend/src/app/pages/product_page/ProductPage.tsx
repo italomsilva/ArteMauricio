54c4e5f6b1bc3926a100a80c575d7aa217a7f55c
italomsilva <italoroyale13@gmail.com>
2025-04-30 08:25:47 -0300

    update

--- frontend/src/app/pages/product_page/ProductPage.tsx
+++ frontend/src/app/pages/product_page/ProductPage.tsx
@@ -38,12 +38,36 @@ export default function ProductPage(props: { id: string }) {
     const titleUrl = encodeURIComponent(`*"${title}"*`);
     const priceUrl = encodeURIComponent(`${price.toFixed(2)}`);
     // const currentUrl = encodeURIComponent(window.location.href);
-    const currentUrl = encodeURIComponent(`https://www.artemauricio.com.br/products/${props.id}`);
+    const currentUrl = encodeURIComponent(
+      `https://www.artemauricio.com.br/products/${props.id}`
+    );
     const numeroWhatsapp = "5585991289947";
 
     const link = `https://wa.me/${numeroWhatsapp}?text=Oi!%20Me%20interessei%20pelo%20produto%20${titleUrl}%20que%20vi%20no%20site,%20com%20o%20preço%20de%20R$%20${priceUrl}.%20Ele%20ainda%20está%20disponível%3F%0A%0AProduto:%20${currentUrl}`;
 
     return link;
+  };
+
+  const handleShare = async (title: string, price: number) => {
+    const url = window.location.href;
+
+    const message = `Oi! Me interessei pelo produto ${title} que vi no site, com o preço de R$ ${price.toFixed(
+      2
+    )}. Ele ainda está disponível?\n\nproduto: ${url}`;
+
+    if (navigator.share) {
+      navigator
+        .share({
+          title: "Produto Interessante",
+          text: message,
+          url,
+        })
+        .then(() => console.log("Compartilhado com sucesso!"))
+        .catch((error) => console.error("Erro ao compartilhar:", error));
+    } else {
+      await navigator.clipboard.writeText(url);
+      alert("Link copiado para a área de transferência!");
+    }
   };
 
   useEffect(() => {
@@ -99,7 +123,9 @@ export default function ProductPage(props: { id: string }) {
             <span>
               <p>id: {product?.id}</p>
               <i>
-                <FaShareNodes />
+                <FaShareNodes
+                  onClick={() => handleShare(product.title, product.price)}
+                />
               </i>
             </span>
           </div>
